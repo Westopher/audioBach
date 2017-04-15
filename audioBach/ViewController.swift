4fdc56944e2bfa89ade65c34b9542ea327aaee4c
West Kraemer <WestKraemer@gmail.com>
2017-04-14 21:14:49 -0400

    addd audio path and player to play mp3 w/o controls

--- audioBach/ViewController.swift
+++ audioBach/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            player.play()
+            
+        } catch {
+        
+            // Process any errors
+    
     }
 
-    override func didReceiveMemoryWarning() {
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -23,3 +38,4 @@ class ViewController: UIViewController {
 
 }
 
+}
